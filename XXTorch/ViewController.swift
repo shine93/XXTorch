536eb626e82bdd55055d6d5722d552d15784d3de
wmx <weimengxue@adesk.com>
2017-08-09 16:20:49 -0700

    Initial Commit

---
+++ XXTorch/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  XXTorch
+//
+//  Created by xhkj on 2017/8/9.
+//  Copyright © 2017年 wmx. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
